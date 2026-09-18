57982a2dd1dd8c9d40d9a43492491a899a02a49f
cohenguillory <cohen.guillory7@gmail.com>
2026-09-18 12:56:34 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -29,22 +35,22 @@ $(function () {
     // TODO 1 - Enable the Grid
     // toggleGrid();
 
-
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(850, 700, 200, 10);
+    createPlatform(650, 600, 200, 10);
+    createPlatform(500, 525, 200, 10);
+    createPlatform(450, 450, 200, 10);
+    createPlatform(300, 400, 200, 10);
+    createPlatform(500, 300, 200, 10);
+    createPlatform(700, 200, 200, 10);
+    createPlatform(950, 250, 200, 10);
 
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 200, 800);
+createCannon("right", 300, 2000);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
